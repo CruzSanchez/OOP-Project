5262dada93f930e6dc996814ddfa564a73cd37ae
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-08-20 17:37:12 -0500

    Update Pizza.cs

--- OOPProject/Pizza.cs
+++ OOPProject/Pizza.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OOPProject
 {
-    class Pizza : Food      //Inhearitance: This class has a name, calories, and total(fat, carbs, and protein).
+    class Pizza : Food      //Inheritance: This class has a name, calories, and total(fat, carbs, and protein).
 	{
 		public int NumberOfPepperoni { get; set; }
 	}
